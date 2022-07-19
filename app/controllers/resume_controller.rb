4b68a670754838ae7756ed88e15e0362343297d2
katywelyczko <katy.welyczko@keysight.com>
2022-07-19 09:57:49 -0600

    update resume download

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,5 +1,5 @@
 class ResumeController < ApplicationController
   def download_pdf
-    send_file "#{Rails.root}/public/kjw_resume.pdf", :disposition => 'inline', type: "application/pdf"
+    send_file "#{Rails.root}/public/kjw_resume1.pdf", :disposition => 'inline', type: "application/pdf"
   end
 end
